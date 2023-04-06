c7960a5662a0f4089b2d801112424750eca8a5fc
tim <tim.d11@gmx.de>
2023-04-06 14:10:49 +0200

    ci: add new project structure

--- src/js/stage/title.ts
+++ src/js/stage/title.ts
@@ -1,5 +1,4 @@
 import {ColorLayer, game, Stage, UITextButton} from 'melonjs';
-import UIContainer from "../entities/menu/UIContainer";
 
 class TitleScreen extends Stage {
   /**
@@ -7,16 +6,6 @@ class TitleScreen extends Stage {
    */
   onResetEvent() {
     // TODO
-    // add a gray background to the default Stage
-    game.world.addChild(new ColorLayer('background', '#202020'));
-
-    const panel = new UIContainer(20, 30, 450, 325);
-
-    panel.addChild(new UITextButton(0, 0, {
-      font: 'PressStart2P', text: "hallo"}));
-
-    game.world.addChild(panel, 1);
-
   }
 
   /**
