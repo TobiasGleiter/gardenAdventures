fb65e1d467fb34849394872f1c4516b88ca371cc
Tobias Kapp <tobias.kapp@diconium.com>
2023-04-19 09:46:26 +0200

    feat: Lvl 4

---
+++ src/data/map/Background_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Background_2" tilewidth="400" tileheight="272" tilecount="1" columns="1">
+ <image source="../img/background/Background_2.png" width="496" height="272"/>
+</tileset>
