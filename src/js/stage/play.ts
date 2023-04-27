b5447080b54afff11bc38a489bdf9c975b14c8b2
Tobias Kapp <kapp.tob@gmail.com>
2023-04-27 12:08:18 +0200

    feat: second level

--- src/js/stage/play.ts
+++ src/js/stage/play.ts
@@ -9,7 +9,7 @@ class PlayScreen extends me.Stage {
    */
   onResetEvent() {
     // load a level
-    me.level.load('enemies');
+    me.level.load('Lvl1-2');
 
     // reset the score
     game.data.score = 0;
@@ -17,12 +17,32 @@ class PlayScreen extends me.Stage {
 
     this.HUD = new UIContainer();
     me.game.world.addChild(this.HUD);
+
+
+    //Test
+     const backgroundImage = new me.Sprite(
+          0,0,
+          {
+            image: me.loader.getImage('Background_2'),
+          }
+        );
+
+        // scale to fit with the viewport size
+        backgroundImage.scale(
+          me.game.viewport.width,
+          me.game.viewport.height
+        );
+
+        // add to the world container
+        me.game.world.addChild(backgroundImage, -3);
   }
   // Run on game resources loaded.
 
   onDestroyEvent(): void {
     me.game.world.removeChild(this.HUD);
   }
+
+
 }
 
 export default PlayScreen;
