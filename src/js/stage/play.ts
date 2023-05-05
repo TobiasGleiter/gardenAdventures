14ad9fc2e637621f7e94c2e418904553f4035460
TobiasGleiter <gleiter.tobias@outlook.de>
2023-05-05 11:41:45 +0200

    fix: background image in play ts

--- src/js/stage/play.ts
+++ src/js/stage/play.ts
@@ -1,7 +1,7 @@
 import * as me from 'melonjs';
 import game from '../../game';
+import network from '../../multiplayer/network';
 import UIContainer from '../UI/HUD';
-import network from "../../multiplayer/network";
 
 class PlayScreen extends me.Stage {
   private HUD: any;
@@ -9,28 +9,29 @@ class PlayScreen extends me.Stage {
    *  action to perform on state change
    */
   update(dt: number): boolean {
-    const pause = new me.BitmapText (me.game.viewport.width/2, me.game.viewport.height/2, {
-      font: 'PressStart2P',
-      text: 'Pause',
-      textAlign: 'center',
-      size: 0.8,
-    });
+    const pause = new me.BitmapText(
+      me.game.viewport.width / 2,
+      me.game.viewport.height / 2,
+      {
+        font: 'PressStart2P',
+        text: 'Pause',
+        textAlign: 'center',
+        size: 0.8,
+      }
+    );
     pause.name = 'pausenname';
 
-    if (me.input.isKeyPressed("pause")) {
-
-      if(me.game.world.getChildByName('pausenname').length == 0){
+    if (me.input.isKeyPressed('pause')) {
+      if (me.game.world.getChildByName('pausenname').length == 0) {
         me.game.world.addChild(pause);
       }
 
       me.timer.setTimeout(() => {
         me.state.pause();
       }, 1);
-
-    }else if (me.state.isPaused() && me.input.isKeyPressed("resume")){
-
+    } else if (me.state.isPaused() && me.input.isKeyPressed('resume')) {
       const pauseChild = me.game.world.getChildByName('pausenname')[0];
-      if (pauseChild && pauseChild.inViewport){
+      if (pauseChild && pauseChild.inViewport) {
         me.game.world.removeChild(pauseChild);
       }
       me.state.resume();
@@ -40,7 +41,7 @@ class PlayScreen extends me.Stage {
 
   onResetEvent() {
     // load a level
-    me.level.load('Lvl1-1');
+    me.level.load('Lvl1-2');
 
     // reset the score
     game.data.score = 0;
@@ -49,34 +50,29 @@ class PlayScreen extends me.Stage {
     this.HUD = new UIContainer();
     me.game.world.addChild(this.HUD);
 
-
     //Test
-     const backgroundImage = new me.Sprite(
-          0,0,
-          {
-            image: me.loader.getImage('Background_2'),
-          }
-        );
+    /*
+    const backgroundImage = new me.Sprite(0, 0, {
+      image: me.loader.getImage('Background_1'),
+    });
 
-        // scale to fit with the viewport size
-        backgroundImage.scale(
-          me.game.viewport.width,
-          me.game.viewport.height
-        );
+    // scale to fit with the viewport size
+    backgroundImage.scale(me.game.viewport.width, me.game.viewport.height);
 
-        // add to the world container
-        me.game.world.addChild(backgroundImage, -3);
+    // add to the world container
+    me.game.world.addChild(backgroundImage, -3);
+    */
   }
   // Run on game resources loaded.
   onDestroyEvent(): void {
     // SEND SCORE TO SERVER IF FINISHED
     let isFinished = true;
-    if(isFinished) {
+    if (isFinished) {
       const res = {
         player: network.getPlayerId() as string, //"123"
         score: game.data.score as number,
-        level: me.level.getCurrentLevel().name as string
-      }
+        level: me.level.getCurrentLevel().name as string,
+      };
       // Send score to server
       network.sendScore(res);
     }
