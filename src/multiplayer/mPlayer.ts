fff0206a50d59300ec0d5752492bb26776c34982
TobiasGleiter <gleiter.tobias@outlook.de>
2023-05-06 16:11:00 +0200

    feat: random colors for pseudoMultiplayers

--- src/multiplayer/mPlayer.ts
+++ src/multiplayer/mPlayer.ts
@@ -17,12 +17,33 @@ class MPlayerEntity extends me.Sprite {
     this.pos.x = x;
     this.pos.y = y;
 
-    this.tint.setColor(0, 255, 0);
+    const rgb = randomColor();
+    console.log(rgb);
+
+    this.tint.setColor(rgb.r, rgb.g, rgb.b);
     this.alpha = 0.5;
 
     this.addAnimation('run', [4]);
     this.setCurrentAnimation('run');
   }
+}
+
+function randomColor() {
+  const colors = [
+    { r: 255, g: 0, b: 0 },
+    { r: 0, g: 255, b: 0 },
+    { r: 0, g: 0, b: 255 },
+    { r: 0, g: 255, b: 255 },
+    { r: 255, g: 0, b: 255 },
+    { r: 255, g: 255, b: 0 },
+  ];
+  // Generate a random index into the array
+  const randomIndex = Math.floor(Math.random() * colors.length);
+
+  const randomColor = colors[randomIndex];
+
+  // Get the color at the random index
+  return randomColor;
 }
 
 export default MPlayerEntity;
